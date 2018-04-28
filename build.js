28465d4353315ceed0923661f120b150ead2cdfd
RedSparr0w <admin@redsparr0w.com>
2018-04-28 19:02:25 +1200

    [build] formatting

--- build.js
+++ build.js
@@ -1,19 +1,18 @@
 var NwBuilder = require('nw-builder');
 var nw = new NwBuilder({
-    files: ['./**','!./bots.json','!./node_modules/nw-builder/**','!./cache/**'], // use the glob format
-    platforms: [ 'win'],
-	buildDir: "../builds",
-	winIco: "./logo.ico",
-	zip: false
+  files: ['./**','!./bots.json','!./node_modules/nw-builder/**','!./cache/**'], // use the glob format
+  platforms: [ 'win'],
+  buildDir: "../builds",
+  winIco: "./logo.ico",
+  zip: false
 });
 
 //Log stuff you want
-
 nw.on('log',  console.log);
 
 // Build returns a promise
 nw.build().then(function () {
-   console.log('Build Complete!');
+  console.log('Build Complete!');
 }).catch(function (error) {
-    console.error(error);
+  console.error(error);
 });
